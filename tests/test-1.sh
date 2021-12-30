c6981a44a360f29502c0296cac44e9bd66cda605
Hadi Foroughmand <mohammad-hadi.foroughmand-araabi@helmholtz-hzi.de>
2021-12-30 22:33:21 +0330

    update sankoff and parallel option is added

--- tests/test-1.sh
+++ tests/test-1.sh
@@ -1,4 +1,5 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${CONDA_PREFIX}/lib/
-mkdir tests/out
+set -x -e 
+mkdir -p tests/out
 build/sankoff --cost tests/data/cost-1.txt --aln tests/data/aln1.txt --tree tests/data/tree1.txt
 build/sankoff --cost tests/data/cost-1.txt --aln tests/data/aln1.txt --tree tests/data/tree1.txt --out-as tests/out/tree-1-no-dep-asr.txt --out-tree tests/out/tree-1-no-dep-tree.txt --ilabel inode 
